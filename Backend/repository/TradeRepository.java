c106b992e167f636429ff64d4fe342cdf494528c
Lamarssom <ggbarabe2@gmail.com>
2025-10-21 16:47:26 +0100

    Update TradeRepository.java

--- Backend/repository/TradeRepository.java
+++ Backend/repository/TradeRepository.java
@@ -3,21 +3,24 @@ package com.musicinvestment.musicapp.repository;
 import com.musicinvestment.musicapp.model.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.param;
-import java.math.BigInteger;
+import org.springframework.data.repository.query.Param;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
     @Query("SELECT SUM(t.ethValue) FROM Trade t WHERE t.artistId = :artistId AND t.timestamp > :since")
-    BigInteger sumEthValueLast24h(String artistId, LocalDateTime since);
+    BigDecimal sumEthValueLast24h(String artistId, LocalDateTime since);
 
-    @Query("SElECT COUNT(t) FROM Trade t WHERE t.artistId = :artistId")
+    @Query("SELECT SUM(t.amountInUsd) FROM Trade t WHERE LOWER (t.buyerOrSeller) = LOWER(:userAddress) AND t.timestamp >= :since")
+    BigDecimal sumAmountInUsdLast24h(@Param("userAddress") String userAddress, @Param("since") LocalDateTime since);
+
+
+    @Query("SELECT COUNT(t) FROM Trade t WHERE t.artistId = :artistId")
     long countByArtistId(String artistId);
 
     Optional<Trade> findByTxHash(String txHash);
 
     List<Trade> findByArtistIdOrderByTimestampAsc(String artistId);
-
 }
