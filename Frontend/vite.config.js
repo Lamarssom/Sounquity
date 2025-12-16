bd0fba514d7cbe75e2f80a2ff7a25d597ff53fac
Lamarssom <ggbarabe2@gmail.com>
2025-12-16 09:26:33 +0100

    Fix startup: updated vite.config.js, optimizeDeps, commonjs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
   plugins: [
