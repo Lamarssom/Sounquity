ace3789707afdfc17d19fe1fc9b95d4a9f1e169f
Lamarssom <ggbarabe2@gmail.com>
2025-12-18 12:03:18 +0100

    SecurityConfig.java,Cors

--- Backend/src/main/java/com/musicinvestment/musicapp/config/SecurityConfig.java
+++ Backend/src/main/java/com/musicinvestment/musicapp/config/SecurityConfig.java
@@ -74,9 +74,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-            "http://localhost:3000")
+            "http://localhost:3000"
             "https://sounquity.vercel.app"
-        );
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
