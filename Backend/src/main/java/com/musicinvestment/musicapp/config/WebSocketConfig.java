e5a65ff967fc6a5ad922f929b75367742655b903
Lamarssom <ggbarabe2@gmail.com>
2026-01-06 18:19:46 +0100

    websocket update

--- Backend/src/main/java/com/musicinvestment/musicapp/config/WebSocketConfig.java
+++ Backend/src/main/java/com/musicinvestment/musicapp/config/WebSocketConfig.java
@@ -18,6 +18,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();  // Endpoint for client connection
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins(
+                "http://localhost:3000",
+                "https://sounquity.vercel.app"
+            )
+            .withSockJS();
     }
 }
