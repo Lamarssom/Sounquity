b96d68a56f6aa85608b38762b464604810bc8d77
Lamarssom <ggbarabe2@gmail.com>
2026-01-29 13:15:16 +0100

    fixed WS & 404 errors

--- Backend/src/main/java/com/musicinvestment/musicapp/config/WebSocketConfig.java
+++ Backend/src/main/java/com/musicinvestment/musicapp/config/WebSocketConfig.java
@@ -19,10 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-            .setAllowedOrigins(
-                "http://localhost:3000",
-                "https://sounquity.vercel.app"
-            );
-            // .withSockJS();
+                .setAllowedOrigins(
+                    "http://localhost:3000",
+                    "https://sounquity.vercel.app"
+                )
+                .withSockJS();
     }
 }
